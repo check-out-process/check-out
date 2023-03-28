c9fb49d32a0e31f1e5a0bca048acc2b36979a1b6
afekl <afekev55@gmail.com>
2023-03-28 13:16:23 +0300

    make list dragable with react-beautiful-dnd

--- src/components/Process/ProcessSectorForm.component.tsx
+++ src/components/Process/ProcessSectorForm.component.tsx
@@ -5,6 +5,7 @@ import { getSectors } from "../../services/Sector.service";
 import AddSectorDrawer from "../Sector/AddSector/AddSectorDrawer.component";
 import SectorCard from "../Sector/SectorCard.component";
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { Droppable, DragDropContext, DropResult } from "react-beautiful-dnd";
 import './ProcessSectorForm.component.css';
 
 const ProcessSectorForm = () => {
@@ -69,28 +70,58 @@ const ProcessSectorForm = () => {
     marginRight: '10px', height: '32px', width: '85px', marginBottom: '6px', marginTop: '8px'
   });
 
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    let add;
+    let active = defaultSectorsData;
+    add = active[source.index];
+    active.splice(source.index, 1);
+    active.splice(destination.index, 0, add);
+
+    setDefaultSectorsData(active);
+  };
+
+
   return (
-    <div style={{ height: '100%' }}>
-      <div className="processSectorForm sectorsList" style={{ display: 'flex', flexDirection: 'column', height: '100%', justifyContent: 'space-between' }}>
-        <div>
-          <List style={{
-            width: '100%', counterReset: 'gradient-counter',
-          }}>
-            {defaultSectorsData.map((sector: Sector) => (
-              <SectorCard sector={sector} removeProcessSector={removeProcessSector} />
-            ))}
-          </List>
-          <IconButton onClick={handleDrawerOpen} style={{ color: '#54546b' }}>
-            <AddCircleOutlineIcon />
-          </IconButton>
-        </div>
-        <div style={{ display: 'flex' }}>
-          <Button variant="outlined" onClick={handleCancel} style={buttonsStyle()} >ביטול</Button>
-          <Button variant="outlined" onClick={saveProcessSector} style={buttonsStyle()}>אישור</Button>
-        </div>
-      </div >
-      <AddSectorDrawer open={open} handleDrawerClose={handleDrawerClose} sectorsData={sectorsData} addProcessSectors={addProcessSectors} />
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div style={{ height: '100%' }}>
+        <div className="processSectorForm sectorsList" style={{ display: 'flex', flexDirection: 'column', height: '100%', justifyContent: 'space-between' }}>
+          <div>
+            <Droppable droppableId="sectorsList">
+              {(provided, snapshot) => (
+                <div
+                  className={`sectors ${snapshot.isDraggingOver ? "dragactive" : ""}`}
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                >
+                  <List style={{
+                    width: '100%', counterReset: 'gradient-counter',
+                  }}>
+                    {defaultSectorsData.map((sector: Sector, index: number) => (
+                      <SectorCard sector={sector} removeProcessSector={removeProcessSector} index={index} />
+                    ))}
+                  </List>
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+            <IconButton onClick={handleDrawerOpen} style={{ color: '#54546b' }}>
+              <AddCircleOutlineIcon />
+            </IconButton>
+          </div>
+          <div style={{ display: 'flex' }}>
+            <Button variant="outlined" onClick={handleCancel} style={buttonsStyle()} >ביטול</Button>
+            <Button variant="outlined" onClick={saveProcessSector} style={buttonsStyle()}>אישור</Button>
+          </div>
+        </div >
+        <AddSectorDrawer open={open} handleDrawerClose={handleDrawerClose} sectorsData={sectorsData} addProcessSectors={addProcessSectors} />
+      </div>
+    </DragDropContext>
 
   )
 }
--- src/components/Sector/SectorCard.component.tsx
+++ src/components/Sector/SectorCard.component.tsx
@@ -4,12 +4,14 @@ import React, { useState, useRef } from 'react';
 import { Sector } from "../../services/models/Sector";
 import './SectorCard.component.css';
 import SectorOptionsModal from "./SectorOptionsModal.component";
+import { Draggable } from "react-beautiful-dnd";
 
 interface ISectorCardProps {
     sector: Sector,
-    removeProcessSector: any
+    removeProcessSector: any,
+    index: number;
 }
-const SectorCard: React.FC<ISectorCardProps> = ({ sector, removeProcessSector }: ISectorCardProps) => {
+const SectorCard: React.FC<ISectorCardProps> = ({ sector, removeProcessSector, index }: ISectorCardProps) => {
     const [open, setOpen] = useState(false);
     const [offsetTop, setOffsetTop] = useState(0);
     const ref = useRef(null)
@@ -22,22 +24,29 @@ const SectorCard: React.FC<ISectorCardProps> = ({ sector, removeProcessSector }:
     }
 
     return (
-        <div>
-            <Card ref={ref} className="cardSector" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', margin: '10px 20px 15px 10px' }}>
-                <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', padding: '0px' }}>
-                    <Typography component="div" variant="h5" style={{ fontSize: '16px', marginRight: '15px' }}>
-                        {sector.name}
-                    </Typography>
-                    <Typography variant="subtitle1" color="textSecondary" component="div" style={{ fontSize: '12px', marginRight: '15px' }}>
-                        {ownerTitle}
-                    </Typography>
-                </CardContent>
-                <IconButton className="iconButton" aria-label="previous" style={{ marginRight: 'auto' }} onClick={(event) => handleOpen(event)}>
-                    <MoreHorizIcon />
-                </IconButton>
-            </Card>
-            <SectorOptionsModal open={open} setOpen={setOpen} refOffsetTop={offsetTop} sector={sector} removeProcessSector={removeProcessSector} />
-        </div>
+        <Draggable key={sector.id} draggableId={sector.id} index={index}>
+            {(provided, snapshot) => (
+                    <div  {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                    className={`sector ${snapshot.isDragging ? "drag" : ""}`}>
+                        <Card ref={ref} className="cardSector" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', margin: '10px 20px 15px 10px' }}>
+                            <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', padding: '0px' }}>
+                                <Typography component="div" variant="h5" style={{ fontSize: '16px', marginRight: '15px' }}>
+                                    {sector.name}
+                                </Typography>
+                                <Typography variant="subtitle1" color="textSecondary" component="div" style={{ fontSize: '12px', marginRight: '15px' }}>
+                                    {ownerTitle}
+                                </Typography>
+                            </CardContent>
+                            <IconButton className="iconButton" aria-label="previous" style={{ marginRight: 'auto' }} onClick={(event) => handleOpen(event)}>
+                                <MoreHorizIcon />
+                            </IconButton>
+                        </Card>
+                        <SectorOptionsModal open={open} setOpen={setOpen} refOffsetTop={offsetTop} sector={sector} removeProcessSector={removeProcessSector} />
+                    </div>
+            )}
+        </Draggable>
     )
 }
 
