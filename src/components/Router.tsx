0179464636f6e1b564b16217a0d51e1c30835830
afekl <afekev55@gmail.com>
2023-04-10 14:18:56 +0300

    change router to start with login

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -7,21 +7,27 @@ import {
 } from "react-router-dom";
 import HomePage from './HomePage/homePage.component';
 import NavBarLayout from './Layout/NavBar/NavBar.component';
+import LogInPage from './LogInPage/logInPage.component';
 import ProcessCreation from './Process/ProcessCreation.component';
 
 const Router = () => {
+  const [logedIn, setLogedIn] = React.useState(false); //get it from context/store that when refresh sty true
 
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<NavBarLayout />}>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/processcreation" element={<ProcessCreation />} />
-      </Route>
+      logedIn ?
+        <Route path="/" element={<NavBarLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/processcreation" element={<ProcessCreation />} />
+        </Route>
+        : <Route >
+          <Route path="/" element={<LogInPage setLogedIn={setLogedIn} />} />
+        </Route>
     )
   )
 
   return (
-    <div style={{height: '100%'}}>
+    <div style={{ height: '100%' }}>
       <RouterProvider router={router} />
     </div>
   );
