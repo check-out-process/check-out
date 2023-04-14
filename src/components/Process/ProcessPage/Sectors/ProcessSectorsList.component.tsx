b9fac50e61b88d72749c36a1ac77052f5eca15c2
afekl <afekev55@gmail.com>
2023-04-14 15:31:28 +0300

    add return icon and useStyles

--- src/components/Process/ProcessPage/Sectors/ProcessSectorsList.component.tsx
+++ src/components/Process/ProcessPage/Sectors/ProcessSectorsList.component.tsx
@@ -1,14 +1,35 @@
-import { CircularProgress, List, Typography } from "@material-ui/core";
+import { CircularProgress, IconButton, List, Typography } from "@material-ui/core";
 import React, { useEffect, useState } from 'react';
 import { ProcessSector } from "../../../../services/models/ProcessSector";
 import { getProcessSectors } from "../../../../services/ProcessSector.service";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import ProcessSectorCard from "./ProcessSectorCard.component";
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { createStyles, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() =>
+    createStyles({
+        headerContainer: {
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '12px'
+        },
+        headerText: {
+            flex: 1
+        },
+        headerIcon: {
+            padding: '0px',
+            marginLeft: '12px'
+        }
+    }),
+);
 
 const ProcessSectorsList: React.FC = () => {
     const [processSectors, setProcessSectors] = useState<ProcessSector[]>();
     const [loading, setLoading] = useState<boolean>(false);
 
+    const classes = useStyles();
+    const navigate = useNavigate();
     const location = useLocation();
     const processId = location.state.processId;
 
@@ -23,8 +44,15 @@ const ProcessSectorsList: React.FC = () => {
             setLoading(false);
         })
     }
+
     return (
         <div>
+            <div className={classes.headerContainer}>
+                <Typography align='center' variant="h5" component="h2" className={classes.headerText}>רשימת הסקטורים</Typography>
+                <IconButton edge="start" onClick={() => navigate(-1)} color="inherit" className={classes.headerIcon}>
+                    <ArrowBackIcon />
+                </IconButton>
+            </div>
             {loading ? <CircularProgress disableShrink /> : null}
             {processSectors?.length > 0 && <List style={{
                 width: '100%',
