c08291a0bb91871aa9ea9e8afd15e2e175bdd02e
afekl <afekev55@gmail.com>
2023-05-20 11:16:45 +0300

    add checkbox for is bed isolation

--- src/components/ProcessCreationPage/ProcessCreationForms/ProcessDynamicProperties.component.tsx
+++ src/components/ProcessCreationPage/ProcessCreationForms/ProcessDynamicProperties.component.tsx
@@ -1,8 +1,7 @@
-import { Accordion, AccordionSummary, makeStyles, createStyles, Theme, AccordionDetails } from '@material-ui/core';
-import ExpandMore from '@material-ui/icons/ExpandMore';
+import { makeStyles, createStyles, Theme, Checkbox } from '@material-ui/core';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize/TextareaAutosize';
 import Typography from '@material-ui/core/Typography';
-import React, { ChangeEventHandler, useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { ProcessCreationDetailsContext } from '../../../context/ProcessCreationContext';
 
 
@@ -31,66 +30,56 @@ const useStyles = makeStyles((theme: Theme) =>
             marginRight: '1%',
             display: 'flex',
             backgroundColor: "transparent"
+        },
+        titleDescription: {
+            marginRight: '1%',
+            marginTop: '1%'
+        },
+        checkBoxBedIsolationContiner: {
+            display: 'flex',
+            alginItems: 'center'
+        },
+        checkBoxBedIsolation: {
+            padding: '0'
+        },
+        textBedIsolation: {
+            marginRight: '1%',
+            marginLeft: '13px'
         }
+
     }),
 );
 
 const ProcessBasicDetailsForm = () => {
-    // const [processProperties, setProcessProperties] = useState<ProcssPropertiesSchema[]>([])
-    // const { processDetails, setProcessDetails } = useContext(ProcessCreationDetailsContext);
     const classes = useStyles();
-
     const { properties, setProperty } = useContext(ProcessCreationDetailsContext);
+    const [checked, setChecked] = React.useState(false);
 
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setChecked(event.target.checked);
+    };
 
-    // useEffect(() => {
-    //     fetchProcessProperties()
-    // }, [])
-
-    // const fetchProcessProperties = () => {
-    //     getProcessPropertiesSchema().then((processProperties: ProcssPropertiesSchema[]) => {
-    //         setProcessProperties(processProperties)
-    //     })
-    // }
-
-    // const onChange = (key: any, value: any): void => {
-    //     processDetails.properties[key] = value
-    //     setProcessDetails({ ...processDetails })
-    // }
 
     const onDescriptionChange = (event: any) => {
-        setProperty('description',event.target.value)
+        setProperty('description', event.target.value)
         console.log(properties)
     }
 
     return (
         <div>
-            <Typography style={{ marginTop: '1%', marginRight: '1%' }} align="right" variant="h6" component="div">תיאור חופשי</Typography>
-            <TextareaAutosize value={properties['description']} minRows={6} placeholder="תיאור" className={classes.textArea} onChange={onDescriptionChange}/>
-
-            {/* <Accordion className={classes.root}>
-                <AccordionSummary style={{borderBottom: '1px solid rgba(0, 0, 0, .125)',boxShadow: '2px 2px 2px 1px rgba(0, 0, 0, 0.2)'}}
-                    expandIcon={<ExpandMore />}
-                >
-                    <Typography style={{ marginRight: '2%' }} align="right" variant="h6" component="h2">הוספת פרטים נוספים על התהליך:</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-
-                    <div style={{ marginTop: "2%",width:'100%' }}>
-                        <Typography style={{ marginTop: '1%', marginRight: '2%' }} align="right" variant="h6" component="h2">תיאור חופשי:</Typography>
-                        <TextareaAutosize minRows={6} placeholder="תיאור" style={{ direction: "rtl", width: '79%', marginRight: '1%', display: 'flex', justifyContent: 'flex-start' }} />
-
-                        <div style={{ marginTop: '1%'}}>
-                            {processProperties.map((property: ProcssPropertiesSchema) => {
-                                return (
-                                    <DynamicPropertiesFactory property={property} onChange={onChange} />
-                                )
-                            })}
-                        </div>
-                    </div>
-                </AccordionDetails>
-            </Accordion> */}
-
+            <div className={classes.checkBoxBedIsolationContiner}>
+                <Typography className={classes.textBedIsolation} align="right" variant="h6" component="div">האם בבידוד</Typography>
+                <Checkbox
+                    className={classes.checkBoxBedIsolation}
+                    size="medium"
+                    color='primary'
+                    checked={checked}
+                    onChange={handleChange}
+                    inputProps={{ 'aria-label': 'controlled' }}
+                />
+            </div>
+            <Typography className={classes.titleDescription} align="right" variant="h6" component="div">תיאור חופשי</Typography>
+            <TextareaAutosize value={properties['description']} minRows={6} placeholder="תיאור" className={classes.textArea} onChange={onDescriptionChange} />
         </div>
     )
 
